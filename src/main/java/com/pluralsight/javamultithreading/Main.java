210c22b0f8023df2713e787d890d7f7b840793df
Jeff Wong <v-jwong1@expedia.com>
2020-01-31 20:33:42 +1000

    use simple arrays and for loop to loop through all 6 files

--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -1,8 +1,6 @@
 package com.pluralsight.javamultithreading;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,20 +8,16 @@ public class Main {
     }
 
     private static void runAdders() {
-        List<Adder> adders = new ArrayList<>();
-        adders.add(new Adder("file1.txt", "fil1-out.txt"));
-        adders.add(new Adder("file2.txt", "fil2-out.txt"));
-        adders.add(new Adder("file3.txt", "fil3-out.txt"));
-        adders.add(new Adder("file4.txt", "fil4-out.txt"));
-        adders.add(new Adder("file5.txt", "fil5-out.txt"));
-        adders.add(new Adder("file6.txt", "fil6-out.txt"));
+        String[] inFiles = { "file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt", "file6.txt" };
+        String[] outFiles = { "file1-out.txt", "file2-out.txt", "file3-out.txt", "file4-out.txt", "file5-out.txt", "file6-out.txt" };
 
-        adders.forEach(a -> {
-            try {
-                a.doAdd();
-            } catch (IOException e) {
-                e.getMessage();
+        try {
+            for (int i = 0; i < inFiles.length; i++) {
+                final Adder adder = new Adder(inFiles[i], outFiles[i]);
+                adder.doAdd();
             }
-        });
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
