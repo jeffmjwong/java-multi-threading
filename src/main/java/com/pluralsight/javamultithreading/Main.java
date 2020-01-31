40979cf17fb79e7a30caa5b3173a9592cfe7e252
Jeff Wong <v-jwong1@expedia.com>
2020-01-31 21:01:08 +1000

    create new thread for each 6 Adders

--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -11,13 +11,10 @@ public class Main {
         String[] inFiles = { "file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt", "file6.txt" };
         String[] outFiles = { "file1-out.txt", "file2-out.txt", "file3-out.txt", "file4-out.txt", "file5-out.txt", "file6-out.txt" };
 
-        try {
-            for (int i = 0; i < inFiles.length; i++) {
-                final Adder adder = new Adder(inFiles[i], outFiles[i]);
-                adder.doAdd();
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        for (int i = 0; i < inFiles.length; i++) {
+            final Adder adder = new Adder(inFiles[i], outFiles[i]);
+            Thread thread = new Thread(adder);
+            thread.start();
         }
     }
 }
