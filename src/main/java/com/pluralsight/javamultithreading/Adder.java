a620707410ebd79c2243cbc1e5391fbe747de900
Jeff Wong <v-jwong1@expedia.com>
2020-01-31 19:25:17 +1000

    create 6 files to be added

--- src/main/java/com/pluralsight/javamultithreading/Adder.java
+++ src/main/java/com/pluralsight/javamultithreading/Adder.java
@@ -1,4 +1,14 @@
 package com.pluralsight.javamultithreading;
 
 public class Adder {
+    private String inFile;
+    private String outFile;
+
+    public Adder(inputFile, outputFile) {
+
+    }
+
+    public void add() {
+
+    }
 }
