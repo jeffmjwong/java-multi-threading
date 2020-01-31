337b75f56cbd1a606d8c83d274e88b545d1bda11
Jeff Wong <v-jwong1@expedia.com>
2020-01-31 21:09:59 +1000

    ensure background threads finished before main thread terminates

--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -1,7 +1,5 @@
 package com.pluralsight.javamultithreading;
 
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) {
         runAdders();
@@ -11,10 +9,20 @@ public class Main {
         String[] inFiles = { "file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt", "file6.txt" };
         String[] outFiles = { "file1-out.txt", "file2-out.txt", "file3-out.txt", "file4-out.txt", "file5-out.txt", "file6-out.txt" };
 
+        Thread[] threads = new Thread[inFiles.length];
+
         for (int i = 0; i < inFiles.length; i++) {
             final Adder adder = new Adder(inFiles[i], outFiles[i]);
-            Thread thread = new Thread(adder);
-            thread.start();
+            threads[i] = new Thread(adder);
+            threads[i].start();
+        }
+
+        try {
+            for (Thread thread : threads) {
+                thread.join();
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
