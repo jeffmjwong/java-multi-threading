d1cb4762a96ca76e59b0c33cd5cc59086482c63b
Jeff Wong <v-jwong1@expedia.com>
2020-01-31 18:41:24 +1000

    update pom file and stub out main method

--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -1,4 +1,7 @@
 package com.pluralsight.javamultithreading;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java multi threading and concurrency!");
+    }
 }
