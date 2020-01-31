e6a39ff0cb88bdc9afd5ccd6504094ebf2f7b3b4
Jeff Wong <v-jwong1@expedia.com>
2020-01-31 20:11:41 +1000

    complete add method in Adder class

--- src/main/java/com/pluralsight/javamultithreading/Adder.java
+++ src/main/java/com/pluralsight/javamultithreading/Adder.java
@@ -1,14 +1,33 @@
 package com.pluralsight.javamultithreading;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Adder {
     private String inFile;
     private String outFile;
 
-    public Adder(inputFile, outputFile) {
-
+    public Adder(String inFile, String outFile) {
+        this.inFile = inFile;
+        this.outFile = outFile;
     }
 
-    public void add() {
+    public void add() throws IOException {
+        int total = 0;
 
+        try (BufferedReader bf = Files.newBufferedReader(Paths.get(inFile))) {
+            String line;
+            while ((line = bf.readLine()) != null) {
+                final int number = Integer.parseInt(line);
+                total += number;
+            }
+        }
+
+        try (BufferedWriter bw = Files.newBufferedWriter(Paths.get(outFile))) {
+            bw.write(total);
+        }
     }
 }
--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -1,7 +1,26 @@
 package com.pluralsight.javamultithreading;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java multi threading and concurrency!");
+
+        try {
+            List<String> numbers = Files.readAllLines(Paths.get("file1.txt"));
+            System.out.println(numbers);
+            int total = numbers
+                    .stream()
+                    .map(Integer::parseInt)
+                    .reduce(Integer::sum)
+                    .get();
+            System.out.println(total);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
