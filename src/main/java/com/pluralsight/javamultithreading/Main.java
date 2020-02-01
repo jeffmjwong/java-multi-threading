7e9d21bc9ab0c855f44531654840eee14495f967
Jeff Wong <v-jwong1@expedia.com>
2020-02-01 15:05:38 +1000

    utilizing thread pool to assign work to threads

--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -1,5 +1,9 @@
 package com.pluralsight.javamultithreading;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
         runAdders();
@@ -9,18 +13,16 @@ public class Main {
         String[] inFiles = { "file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt", "file6.txt" };
         String[] outFiles = { "file1-out.txt", "file2-out.txt", "file3-out.txt", "file4-out.txt", "file5-out.txt", "file6-out.txt" };
 
-        Thread[] threads = new Thread[inFiles.length];
+        ExecutorService es = Executors.newFixedThreadPool(3);
 
         for (int i = 0; i < inFiles.length; i++) {
             final Adder adder = new Adder(inFiles[i], outFiles[i]);
-            threads[i] = new Thread(adder);
-            threads[i].start();
+            es.submit(adder);
         }
 
         try {
-            for (Thread thread : threads) {
-                thread.join();
-            }
+            es.shutdown();
+            es.awaitTermination(60, TimeUnit.SECONDS);
         } catch (InterruptedException e) {
             System.out.println(e.getMessage());
         }
