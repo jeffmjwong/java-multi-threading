714f38b89c9312622484b3d29f4ebc7c6e5a3188
Jeff Wong <v-jwong1@expedia.com>
2020-02-01 17:15:54 +1000

    complete BankAccount class

--- src/main/java/com/pluralsight/javamultithreading/BankAccount.java
+++ src/main/java/com/pluralsight/javamultithreading/BankAccount.java
@@ -1,4 +1,17 @@
 package com.pluralsight.javamultithreading;
 
 public class BankAccount {
+    private int balance;
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public BankAccount(int balance) {
+        this.balance = balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
 }
