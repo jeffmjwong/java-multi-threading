2b30f3138551aab76164c3367830b70c208cbb98
Jeff Wong <v-jwong1@expedia.com>
2020-02-01 17:09:15 +1000

    create BankAccount class

--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
