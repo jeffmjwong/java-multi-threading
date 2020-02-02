3c8f2c62078b4539ae734623d08e645017771c9c
Jeff Wong <v-jwong1@expedia.com>
2020-02-02 18:20:10 +1000

    stub out HighVolumeAccount with some methods and constructors

--- src/main/java/com/pluralsight/javamultithreading/HighVolumeAccount.java
+++ src/main/java/com/pluralsight/javamultithreading/HighVolumeAccount.java
@@ -1,4 +1,18 @@
 package com.pluralsight.javamultithreading;
 
-public class HighVolumeAccount {
+public class HighVolumeAccount extends BankAccount {
+    public HighVolumeAccount(String id) {
+        super(id);
+    }
+    public HighVolumeAccount(String id, int balance) {
+        super(id, balance);
+    }
+
+    private int[] readDailyDeposits() {
+
+    }
+
+    private int[] readDailyWithdrawals() {
+
+    }
 }
