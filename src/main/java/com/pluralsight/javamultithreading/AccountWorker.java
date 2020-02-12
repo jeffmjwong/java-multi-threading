bde8401f02cd3623c803e2ab5a65e80ace1c7489
Jeff Wong <v-jwong1@expedia.com>
2020-02-12 19:26:09 +1000

    stub out AccountWorker class

--- src/main/java/com/pluralsight/javamultithreading/AccountWorker.java
+++ src/main/java/com/pluralsight/javamultithreading/AccountWorker.java
@@ -1,4 +1,29 @@
 package com.pluralsight.javamultithreading;
 
-public class AccountWorker {
+public class AccountWorker implements Runnable {
+    BankAccount ba;
+    HighVolumeAccount hva;
+
+    public AccountWorker(BankAccount ba) {
+
+    }
+    public AccountWorker(HighVolumeAccount hva) {
+
+    }
+
+    public void doWork() {
+        Thread t = new Thread(hva != null ? hva : this);
+    }
+
+    @Override
+    public void run() {
+        char transactionType = 'd';
+        int amount = 100;
+
+        if (transactionType == 'w') {
+            ba.withdrawal(amount);
+        } else {
+            ba.deposit(amount);
+        }
+    }
 }
