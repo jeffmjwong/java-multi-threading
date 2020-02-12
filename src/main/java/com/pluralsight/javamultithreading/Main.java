7ee6f56676402f797da84e91d6616bce27fa17d1
Jeff Wong <v-jwong1@expedia.com>
2020-02-12 19:19:45 +1000

    create AccountWorker class

--- src/main/java/com/pluralsight/javamultithreading/Main.java
+++ src/main/java/com/pluralsight/javamultithreading/Main.java
@@ -11,7 +11,7 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) {
 //        runAdders();
 //        runBankAccounts();
 
