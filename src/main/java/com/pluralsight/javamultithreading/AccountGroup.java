2d5ec525d8c864b21a98438f212c80f945fa57ab
Jeff Wong <v-jwong1@expedia.com>
2020-02-16 16:56:59 +1000

    finish up AccountGroup class

--- src/main/java/com/pluralsight/javamultithreading/AccountGroup.java
+++ src/main/java/com/pluralsight/javamultithreading/AccountGroup.java
@@ -1,4 +1,18 @@
 package com.pluralsight.javamultithreading;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AccountGroup {
+    private Map<String, BankAccount> accountMap = new HashMap<>();
+    private int totalBalance;
+
+    public int getTotalBalance() {
+        return totalBalance;
+    }
+
+    public void addAccount(BankAccount bankAccount) {
+        totalBalance += bankAccount.getBalance();
+        accountMap.put(bankAccount.getId(), bankAccount);
+    }
 }
