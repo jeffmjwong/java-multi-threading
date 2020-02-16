63174ed87b3f85ef67574db3f5ad0275140aaa9c
Jeff Wong <v-jwong1@expedia.com>
2020-02-16 16:58:34 +1000

    mark totalBalance in AccountGroup as transient

--- src/main/java/com/pluralsight/javamultithreading/AccountGroup.java
+++ src/main/java/com/pluralsight/javamultithreading/AccountGroup.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class AccountGroup implements Serializable {
     private Map<String, BankAccount> accountMap = new HashMap<>();
-    private int totalBalance;
+    private transient int totalBalance;
 
     public int getTotalBalance() {
         return totalBalance;
