f2c6f1781d854a2e3a8fa9cdd3212db4ce0e6ca6
Jeff Wong <v-jwong1@expedia.com>
2020-02-16 16:57:47 +1000

    implements Serializable for AccountGroup class

--- src/main/java/com/pluralsight/javamultithreading/AccountGroup.java
+++ src/main/java/com/pluralsight/javamultithreading/AccountGroup.java
@@ -1,9 +1,10 @@
 package com.pluralsight.javamultithreading;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class AccountGroup {
+public class AccountGroup implements Serializable {
     private Map<String, BankAccount> accountMap = new HashMap<>();
     private int totalBalance;
 
